05b394df63043df995eac3855cb4606f230dad71
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-12-21 22:29:44 +0900

    Revert "プレゼントを消すとプレゼントが出るように"
    This reverts commit 47f235dcc27a11660609202c05b6cbd761230d1c.
    
    画像全削除

--- Game/BackDrop.cs
+++ Game/BackDrop.cs
@@ -3,34 +3,9 @@ using Avalonia.Media;
 using Avalonia;
 using Avalonia.Controls.Shapes;
 using System;
-using System.Collections.Generic;
 
 public class BackDrop : Canvas
 {
-    private readonly List<ItemImage> itemImages =
-    [
-        new("さんた", "christmas_ornament01_santa.png"),
-        new ItemImage("はなわ", "christmas_ornament02_wreath.png"),
-        new ItemImage("ゆきだるま", "christmas_ornament03_snowman.png"),
-        new ItemImage("ゆきだるま", "christmas_ornament04_snowman.png"),
-        new ItemImage("ぷれぜんと", "christmas_ornament05_present.png"),
-        new ItemImage("ぷれぜんと", "christmas_ornament06_present.png"),
-        new ItemImage("つりー", "christmas_ornament07_tree.png"),
-        new ItemImage("くつした", "christmas_ornament08_sock.png"),
-        new ItemImage("すず", "christmas_ornament09_bells.png"),
-        new ItemImage("すず", "christmas_ornament10_bell.png"),
-        new ItemImage("ほし", "christmas_ornament11_star.png"),
-        new ItemImage("あめ", "christmas_ornament12_candy.png"),
-        new ItemImage("ろうそく", "christmas_ornament13_candle.png"),
-        new ItemImage("ひいらぎ", "christmas_ornament14_hiiragi.png"),
-        new ItemImage("まつぼっくり", "christmas_ornament15_matsubokkuri.png"),
-        new ItemImage("となかい", "christmas_ornament16_tonakai.png"),
-        new ItemImage("ほし", "christmas_ornament17_topstar.png"),
-        new ItemImage("すたー", "christmas_ornament17_topstar.png"),
-    ];
-
-   private String Imagedirectory = "Assets";
-  
     public BackDrop()
     {
 
@@ -48,51 +23,16 @@ public class BackDrop : Canvas
 
         // CanvasにRectangleを追加
         Children.Add(rectangle);
+
+        // サイズ変更イベントの登録
+        this.SizeChanged += OnSizeChanged;
     }
-    public void AddBackItem(string word)
+
+    private void OnSizeChanged(object sender, SizeChangedEventArgs e)
     {
-        // 名前で一致するアイテムを検索
-        var matchedItem = itemImages.Find(item => item.Name == word);
-        if (matchedItem == null)
-        {
-            Console.WriteLine($"Item with name '{word}' not found.");
-            return;
-        }
-
-        // 画像のファイルパスを作成
-        var filePath = System.IO.Path.Combine(Imagedirectory, matchedItem.FileName);
-
-        // 画像を読み込み
-        var image = new Image
-        {
-            Source = new Avalonia.Media.Imaging.Bitmap(filePath),
-            Width = 100, // 必要に応じてサイズ調整
-            Height = 100
-        };
-
-        // ランダムな位置に配置
-        var random = new Random();
-        SetLeft(image, random.Next(0, 1100));
-        SetTop(image, random.Next(0,650));
-
-        // Canvasに追加
-        Children.Add(image);
-    }
-    public void Reset(){
-        Children.Clear();
-        Update();
-    }
-}
-
-
-class ItemImage
-{
-    public string Name { get; }
-    public string FileName { get; }
-
-    public ItemImage(string name, string fileName)
-    {
-        Name = name;
-        FileName = fileName;
+        // 画面サイズに合わせてRectangleを調整
+        var rectangle = Children[0] as Rectangle;
+        rectangle.Width = e.NewSize.Width;
+        rectangle.Height = e.NewSize.Height;
     }
 }
